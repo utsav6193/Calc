287b8fc0f3304072699e66db130413ea938f718f
Utsav Parikh <utsav6193@gmail.com>
2016-02-11 15:00:51 -0800

    Performance improvement

--- CalC/ViewController.swift
+++ CalC/ViewController.swift
@@ -104,7 +104,7 @@ class ViewController: UIViewController {
     
     func updateCalculatorDisplay() {
         // If the value is an integer, don't show a decimal point
-        let intCalcVal = Int(calcVal)
+        let intCalcVal = UInt64(calcVal)
         if calcVal - Double(intCalcVal) == 0 {
             resultField.text = "\(intCalcVal)"
         } else {
